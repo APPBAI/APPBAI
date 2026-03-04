6e23bd00e1d6186bd4f590ec7eb7767358b72786
Elvis the builder <165419716+elvisthebuilder@users.noreply.github.com>
2026-03-04 15:02:40 +0000

    Remove SpeedInsights component from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,7 +113,7 @@ export default function RootLayout({
     return (
         <html lang="en" className={`${spaceGrotesk.variable} ${inter.variable}`}>
             <body>
-                <SpeedInsights />
+                
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
